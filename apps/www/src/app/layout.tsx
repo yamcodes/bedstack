02497bc92b5f3aa36e27c010ac1736ae7b9a22cc
Yam C Borodetsky <yam@yam.codes>
2026-01-18 03:13:28 +0500

    feat: Update navigation title layout, switch to Geist and JetBrains Mono fonts, and refine global navigation styles.

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,11 +1,16 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { ThemeProvider } from '@/providers/theme';
+import { Geist, JetBrains_Mono } from 'next/font/google';
 
-const inter = Inter({
+const geist = Geist({
+  subsets: ['latin'],
+});
+
+const mono = JetBrains_Mono({
+  variable: '--font-mono',
   subsets: ['latin'],
 });
 
@@ -29,7 +34,11 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${geist.className} ${mono.variable}`}
+      suppressHydrationWarning
+    >
       <body className="flex flex-col min-h-screen">
         <ThemeProvider>
           <RootProvider theme={{ enableSystem: true }}>{children}</RootProvider>
